d9e8eb3fff139c3529a7388c49bb6c6d5b7c412a
Romy Jansen <R.Jansen19@student.han.nl>
2023-04-12 12:05:23 +0200

    opzetje testproject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [grid, setGrid] = useState(Array.from({ length: 5 }, () => Array.from({ length: 5 }, () => "empty")));
+
+    const handleCellClick = (row, col) => {
+        const newGrid = [...grid];
+        // toggle the cell between "empty" and "tree" roles
+        newGrid[row][col] = newGrid[row][col] === "empty" ? "tree" : "empty";
+        setGrid(newGrid);
+        // send a POST request to the backend to update the role of the cell
+        fetch("http://localhost:8000/update-cell", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ row, col, role: newGrid[row][col] })
+        });
+    };
+
+    return (
+        <div className="App">
+            <table>
+                <tbody>
+                {grid.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {row.map((cell, colIndex) => (
+                            <td
+                                key={colIndex}
+                                onClick={() => handleCellClick(rowIndex, colIndex)}
+                                className={cell}
+                                style={{ backgroundColor: cell === "tree" ? "green" : "white" }}
+                            />
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
-
-export default App;
