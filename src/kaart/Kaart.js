8c09c504fc2c186a3bd2138d90acda810f301ed9
Romy Jansen <R.Jansen19@student.han.nl>
2023-06-15 15:17:09 +0200

    Created branch for testing Fabric.js

---
+++ src/kaart/Kaart.js
@@ -0,0 +1,23 @@
+
+
+export function Kaart(){
+// reference canvas element (with id="c")
+    var canvasEl = document.getElementById('c');
+
+// get 2d context to draw on (the "bitmap" mentioned earlier)
+    var ctx = canvasEl.getContext('2d');
+
+// set fill color of context
+    ctx.fillStyle = 'red';
+
+// create rectangle at a 100,100 point, with 20x20 dimensions
+    ctx.fillRect(100, 100, 20, 20);
+
+
+    return(
+        <div>
+            <canvas id={"c"}></canvas>
+            <p>Hier komt de kaart!</p>
+        </div>
+    )
+}
