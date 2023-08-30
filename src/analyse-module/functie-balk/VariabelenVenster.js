21f2fa263d81c0a089f09ba449504f8d3af4a455
Romy Jansen <R.Jansen19@student.han.nl>
2023-08-30 22:07:43 +0200

    commit to change workstations

--- src/analyse-module/functie-balk/VariabelenVenster.js
+++ src/analyse-module/functie-balk/VariabelenVenster.js
@@ -18,56 +18,28 @@ export function VariabelenVenster(){
         }
     }
 
-    // useEffect(() => {
-    //     let isMounted = true;
-    //     fetchData();
-    //     return () => {
-    //         isMounted = false;
-    //     };
-    //
-    //     // simulate some Web API fetching
-    //     function fetchData() {
-    //         setTimeout(async () => {
-    //             // drop "if (isMounted)" to trigger error again
-    //             // (take IDE, doesn't work with stack snippet)
-    //             if (isMounted) setAfstanden(await getAllAfstanden())
-    //             else console.log("aborted setState on unmounted component")
-    //         }, 4000);
-    //     }
-    // }, []);
+    useEffect(() => {
+        setVariabelen(getAfstanden())
+    }, [])
 
-    // async function getData() {
-    //     setVariabelen(await getAllData());
-    // }
-    //
     async function getAfstanden(){
         setAfstanden(await getAllAfstanden())
     }
 
-    // function get_variabelen(){
-    //     var data = getAllData();
-    //     // eslint-disable-next-line no-restricted-globals
-    //     var newVar = new Variabele(data, data[0][1], 0);
-    //     let varList = [];
-    //     varList.push(newVar);
-    //     setVariabelen(varList);
+    // const mockVars = [];
+    // for (let i = 0; i < 10; i++) {
+    //     mockVars.push(new Variabele(i, "name" + i, i*i))
     // }
-
-    const mockVars = [];
-    for (let i = 0; i < 10; i++) {
-        mockVars.push(new Variabele(i, "name" + i, i*i))
-    }
 
     return (
         <div id={"variabelen-venster"}>
             <p>*variabelen*</p>
-            {getAfstanden()}
             {/*{mockVars && mockVars.map((vars,index) => (*/}
             {/*    <p>{vars.name} - {vars.value}</p>*/}
             {/*))}*/}
-            {/*{afstanden && afstanden.map((vars, index) => (*/}
-            {/*    <p>{vars.naam} - {vars.id}</p>*/}
-            {/*))}*/}
+            {(afstanden !== "loading...") && afstanden.map((vars, index) => (
+                <p>{vars.id} - {vars.naam}</p>
+            ))}
             <p>{JSON.stringify(afstanden)}</p>
             <p>{JSON.stringify(variabelen)}</p>
         </div>
